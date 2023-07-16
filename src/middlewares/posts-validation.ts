bcf457efdde193be8202eecec4e71c3692c6166a
Muslim-Abubakr <ismusis@yandex.ru>
2023-07-16 15:28:07 +0300

    added

--- src/middlewares/posts-validation.ts
+++ src/middlewares/posts-validation.ts
@@ -6,14 +6,17 @@ export const validationCreateUpdatePost = [
    body('title').isString().trim().isLength({min: 1, max: 30}).withMessage('incorrect title'),
    body('shortDescription').isString().trim().isLength({min: 1, max: 100}).withMessage('incorrect shortDescription'),
    body('content').isString().trim().isLength({min: 1, max: 1000}).withMessage('incorrect content'),
-   body('blogId').isString().trim().notEmpty().withMessage('incorrect blogId').custom((id) => {
-      const foundPost = db.posts.find(b => b.id === id)
-
-      if (!foundPost) {
-         throw new Error('Blog not found')
-      } else {
-         return true
-      }
-   }),
+   body('blogId').isString().notEmpty().trim().withMessage('incorrect blogId'),
    inputValidationMiddleware
 ]
+
+
+// .custom((id) => {
+//    const foundPost = db.posts.find(b => b.id === id)
+
+//    if (!foundPost) {
+//       throw new Error('Blog not found')
+//    } else {
+//       return true
+//    }
+// })
--- src/routes/posts-router.ts
+++ src/routes/posts-router.ts
@@ -25,35 +25,35 @@ postsRouter.get('/:id', (req: Request, res: Response) => {
 })
 
 postsRouter.post('/',
-authorizationMiddleware, 
-validationCreateUpdatePost,
-(req: Request, res: Response) => {
-    const { title, shortDescription, content, blogId, blogName } = req.body
-    const newPost = postsRepository.createPost(title, shortDescription, content, blogId, blogName)
-    
-    res
-        .status(201)
-        .send(newPost)
+    authorizationMiddleware, 
+    validationCreateUpdatePost,
+    (req: Request, res: Response) => {
+        const { title, shortDescription, content, blogId, blogName } = req.body
+        const newPost = postsRepository.createPost(title, shortDescription, content, blogId, blogName)
+        
+        res
+            .status(201)
+            .send(newPost)
 })
 
 postsRouter.put('/:id',
-authorizationMiddleware,
-validationCreateUpdatePost,
-(req: Request, res: Response) => {
-    const { title, shortDescription, content, blogId, blogName } = req.body
-    const isUpdated = postsRepository.updatePost(req.params.id, title, shortDescription, content, blogId, blogName)
+    authorizationMiddleware,
+    validationCreateUpdatePost,
+    (req: Request, res: Response) => {
+        const { title, shortDescription, content, blogId, blogName } = req.body
+        const isUpdated = postsRepository.updatePost(req.params.id, title, shortDescription, content, blogId, blogName)
 
-    if (isUpdated) {
-        const post = postsRepository.getPostsById(req.params.id)
-        res.send(post)
-    } else {
-        res.status(404)
-    }
+        if (isUpdated) {
+            const post = postsRepository.getPostsById(req.params.id)
+            res.send(post)
+        } else {
+            res.status(404)
+        }
 })
 
 postsRouter.delete('/:id',
-authorizationMiddleware,
-(req: Request, res: Response) => {
-    const filteredPost = postsRepository.deletePost(req.params.id)
-    filteredPost ? res.sendStatus(204): res.sendStatus(404)
+    authorizationMiddleware,
+    (req: Request, res: Response) => {
+        const filteredPost = postsRepository.deletePost(req.params.id)
+        filteredPost ? res.sendStatus(204): res.sendStatus(404)
 })
