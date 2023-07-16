4e53d87916cec31e5b9c629c367e78b51e2658b2
Muslim-Abubakr <ismusis@yandex.ru>
2023-07-16 18:31:31 +0300

    added

--- src/middlewares/blogs-validation.ts
+++ src/middlewares/blogs-validation.ts
@@ -2,7 +2,7 @@ import { body } from "express-validator/src/middlewares/validation-chain-builder
 import { inputValidationMiddleware } from "./input-validation-middlewares";
 
 export const validationCreateUpdateBlog = [
-    body('name').isString().trim().isLength({min: 1, max: 500}).withMessage('incorrect name'),
+    body('name').isString().trim().isLength({min: 1, max: 15}).withMessage('incorrect name'),
     body('description').isString().trim().isLength({min: 1, max: 500}).withMessage('incorrect description'),
     body('websiteUrl').isURL().isLength({min: 1, max: 100}).withMessage('incorrect website URL'),
     inputValidationMiddleware
