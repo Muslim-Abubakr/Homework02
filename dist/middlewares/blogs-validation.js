27f1f08b8a03f10fb21e46e5699e4f3ca3264f1f
Muslim-Abubakr <ismusis@yandex.ru>
2023-07-17 18:03:41 +0300

    added

--- dist/middlewares/blogs-validation.js
+++ dist/middlewares/blogs-validation.js
@@ -4,7 +4,11 @@ exports.validationCreateUpdateBlog = void 0;
 const validation_chain_builders_1 = require("express-validator/src/middlewares/validation-chain-builders");
 exports.validationCreateUpdateBlog = [
     (0, validation_chain_builders_1.body)('name').notEmpty().withMessage('name is required'),
-    (0, validation_chain_builders_1.body)('name').trim().isString().isLength({ min: 1, max: 15 }).withMessage('')
+    (0, validation_chain_builders_1.body)('name').isString().trim().isLength({ min: 1, max: 15 }).withMessage('name should not be empty and length min 1 max 15'),
+    (0, validation_chain_builders_1.body)('description').notEmpty().withMessage('description is required'),
+    (0, validation_chain_builders_1.body)('description').isString().trim().isLength({ min: 1, max: 500 }).withMessage('description should not be empty and length min 1 max 500'),
+    (0, validation_chain_builders_1.body)('websiteUrl').notEmpty().withMessage('websiteUrl is required'),
+    (0, validation_chain_builders_1.body)('websiteUrl').isString().trim().isLength({ min: 1, max: 100 }).withMessage('websiteUrl should not be empty and length min 1 max 100')
     // body('name').isString().trim().isLength({min: 1, max: 15}).withMessage('incorrect name'),
     // body('description').isString().trim().isLength({min: 1, max: 500}).withMessage('incorrect description'),
     // body('websiteUrl').isURL().isLength({min: 1, max: 100}).withMessage('incorrect website URL'),
--- src/middlewares/blogs-validation.ts
+++ src/middlewares/blogs-validation.ts
@@ -3,8 +3,12 @@ import { inputValidationMiddleware } from "./input-validation-middlewares";
 
 export const validationCreateUpdateBlog = [
     body('name').notEmpty().withMessage('name is required'),
-    body('name').trim().isString().isLength({min: 1, max:15}).withMessage('')
-
+    body('name').isString().trim().isLength({min: 1, max:15}).withMessage('name should not be empty and length min 1 max 15'),
+    body('description').notEmpty().withMessage('description is required'),
+    body('description').isString().trim().isLength({min: 1, max: 500}).withMessage('description should not be empty and length min 1 max 500'),
+    body('websiteUrl').notEmpty().withMessage('websiteUrl is required'),
+    body('websiteUrl').isString().trim().isLength({min: 1, max: 100}).withMessage('websiteUrl should not be empty and length min 1 max 100')
+    
     // body('name').isString().trim().isLength({min: 1, max: 15}).withMessage('incorrect name'),
     // body('description').isString().trim().isLength({min: 1, max: 500}).withMessage('incorrect description'),
     // body('websiteUrl').isURL().isLength({min: 1, max: 100}).withMessage('incorrect website URL'),
