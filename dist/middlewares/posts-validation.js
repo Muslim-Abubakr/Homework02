5aa389178b9c94835aedf2e4d8345ccce65dd68b
Muslim-Abubakr <ismusis@yandex.ru>
2023-11-28 22:46:02 +0300

    add mongoURL

--- dist/middlewares/posts-validation.js
+++ dist/middlewares/posts-validation.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.validationCreateUpdatePost = void 0;
 const validation_chain_builders_1 = require("express-validator/src/middlewares/validation-chain-builders");
@@ -8,14 +17,14 @@ exports.validationCreateUpdatePost = [
     (0, validation_chain_builders_1.body)('title').notEmpty().isString().trim().isLength({ min: 1, max: 30 }).withMessage('title should not be empty and length min 1 - max 30'),
     (0, validation_chain_builders_1.body)('shortDescription').notEmpty().isString().trim().isLength({ min: 1, max: 100 }).withMessage('shortDescription should not be empty and length min 1 - max 100'),
     (0, validation_chain_builders_1.body)('content').trim().notEmpty().isString().isLength({ min: 1, max: 1000 }).withMessage('content should not be empty and length min 1 - max 1000'),
-    (0, validation_chain_builders_1.body)('blogId').notEmpty().isString().trim().withMessage('blogId should be a string').custom(id => {
-        const foundBlog = blogs_repository_1.blogsRepository.getBlogsById(id);
-        if (!foundBlog) {
+    (0, validation_chain_builders_1.body)('blogId').notEmpty().isString().trim().withMessage('blogId should be a string').custom((id) => __awaiter(void 0, void 0, void 0, function* () {
+        const foundBlogById = yield blogs_repository_1.blogsRepository.getBlogsById(id);
+        if (!foundBlogById) {
             throw new Error('blog for specific post not found');
         }
         else {
             return true;
         }
-    }),
+    })),
     input_validation_middlewares_1.inputValidationMiddleware
 ];
--- src/middlewares/posts-validation.ts
+++ src/middlewares/posts-validation.ts
@@ -6,10 +6,10 @@ export const validationCreateUpdatePost = [
    body('title').notEmpty().isString().trim().isLength({min: 1, max: 30}).withMessage('title should not be empty and length min 1 - max 30'),
    body('shortDescription').notEmpty().isString().trim().isLength({min: 1, max: 100}).withMessage('shortDescription should not be empty and length min 1 - max 100'),
    body('content').trim().notEmpty().isString().isLength({min: 1, max: 1000}).withMessage('content should not be empty and length min 1 - max 1000'),
-   body('blogId').notEmpty().isString().trim().withMessage('blogId should be a string').custom(id => {
-       const foundBlog = blogsRepository.getBlogsById(id)
+   body('blogId').notEmpty().isString().trim().withMessage('blogId should be a string').custom(async (id) => {
+       const foundBlogById = await blogsRepository.getBlogsById(id)
 
-       if (!foundBlog) {
+       if (!foundBlogById) {
           throw new Error('blog for specific post not found')
        } else {
           return true 
